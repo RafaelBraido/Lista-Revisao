d88f773671e6a842f3136ccbdcd93f832ab3cd28
RafaelBraido <rafaelbraido126@gmail.com>
2026-04-22 15:26:20 -0300

    first commit

---
+++ ex01.js
@@ -0,0 +1,6 @@
+let aura = [1,2,3,4,5,7]
+let ProMax = 0
+for(let i = 0 ;i < aura.length; i++){
+   ProMax = ProMax + aura[i]; 
+}
+ console.log(ProMax);
---
+++ ex02.js
@@ -0,0 +1,8 @@
+let Terno = [5, 8, 11, 6];
+let Fino = 0;
+for (let i = 0; i < Terno.length; i++) {
+    if (Terno[i] % 2 !== 0) {
+        Fino++;
+    }
+}
+console.log(Fino);
